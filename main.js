5fcfaa3d2a1be27670d71131fb0af8b4437baa4c
JAlmarza98 <j.almarza98@gmail.com>
2023-10-04 18:08:36 +0200

    game loop basico

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ canvas.width = BLOCK_SIZE * BOARD_WIDTH
 canvas.height = BLOCK_SIZE * BOARD_HEIGHT
 
 context.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+// Game loop
+function update () {
+  draw()
+  window.requestAnimationFrame(update)
+}
+
+function draw () {
+  context.fillStyle = '#000'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+update()
